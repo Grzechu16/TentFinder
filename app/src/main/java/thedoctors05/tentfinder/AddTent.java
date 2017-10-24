e438f4e243f6cfe6e89ec6ebaf194643654b8f69
Gregory <grzegorzkijas@outlook.com>
2017-10-24 23:03:29 +0200

    permission

--- app/src/main/java/thedoctors05/tentfinder/AddTent.java
+++ app/src/main/java/thedoctors05/tentfinder/AddTent.java
@@ -1,8 +1,12 @@
 package thedoctors05.tentfinder;
 
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -43,12 +47,23 @@ public class AddTent extends AppCompatActivity {
 
 
     private void getPositon(TextView view1, TextView view2) {
-        criteria = new Criteria();
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        positionProvider = locationManager.getBestProvider(criteria, true);
-        location = locationManager.getLastKnownLocation(positionProvider);
-        view1.setText("Longitude: " + location.getLongitude());
-        view2.setText("Longitude: " + location.getLatitude());
-    }
 
+        //Request permission from the user
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+
+        //Checking permission for SDK >= 23
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            criteria = new Criteria();
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            positionProvider = locationManager.getBestProvider(criteria, true);
+            location = locationManager.getLastKnownLocation(positionProvider);
+            view1.setText("Longitude: " + location.getLongitude());
+            view2.setText("Longitude: " + location.getLatitude());
+        }
+    }
 }
