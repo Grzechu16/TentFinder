f7dc8a9e68a58ed7966c85e8587ce06ec2cbac87
Gregory <grzegorzkijas@outlook.com>
2017-10-24 23:09:49 +0200

    permission

--- app/src/main/java/thedoctors05/tentfinder/AddTent.java
+++ app/src/main/java/thedoctors05/tentfinder/AddTent.java
@@ -9,6 +9,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -64,6 +65,13 @@ public class AddTent extends AppCompatActivity {
             location = locationManager.getLastKnownLocation(positionProvider);
             view1.setText("Longitude: " + location.getLongitude());
             view2.setText("Longitude: " + location.getLatitude());
+        } else {
+            Log.d("debugging", "Permission checked: NOK");
+            //Request permission from the user
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            }
+            Log.d("debugging", "Permission requested.");
         }
     }
 }
