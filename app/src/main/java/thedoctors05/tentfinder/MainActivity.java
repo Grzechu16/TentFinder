3cd37f154ed7b54fa2b26dc4aa74952001bb5169
Gruszecki <wojtek.gruszecki@gmail.com>
2017-10-24 19:10:52 +0200

    Reading a localisation v0.5

--- app/src/main/java/thedoctors05/tentfinder/MainActivity.java
+++ app/src/main/java/thedoctors05/tentfinder/MainActivity.java
@@ -1,15 +1,52 @@
 package thedoctors05.tentfinder;
 
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView provider, longitude, latitude;
+    LocationManager lm;
+    Criteria cr;
+    Location loc;
+    String bestProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        provider = (TextView) findViewById(R.id.dostawca_ety);
+        longitude = (TextView) findViewById(R.id.dlugosc_ety);
+        latitude = (TextView) findViewById(R.id.szerokosc_ety);
+
+        //Request permission from the user
+        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( this, new String[] {android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+
+        //Checking permission for SDK >= 23
+        if ( Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            cr = new Criteria();    //Provider criteria, e.g. power consumption or cost
+            lm = (LocationManager) getSystemService(LOCATION_SERVICE);      //Via getSystemService you can manage other sensors such as temperature sensor, pressure sensor
+            bestProvider = lm.getBestProvider(cr, true);    //Name of the best provider considering criteria, true = only active providers
+            loc = lm.getLastKnownLocation(bestProvider);
+
+            //Setting text
+            provider.setText(bestProvider);
+            longitude.setText("" + loc.getLongitude());
+            latitude.setText("" + loc.getLatitude());
+        }
 
     }
 }
