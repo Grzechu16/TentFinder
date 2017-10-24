9dcf2cc0eabcab4d2fcf3fef3b9291b42aa23af3
Gregory <grzegorzkijas@outlook.com>
2017-10-24 20:35:26 +0200

    test

--- app/src/main/java/thedoctors05/tentfinder/MainActivity.java
+++ app/src/main/java/thedoctors05/tentfinder/MainActivity.java
@@ -1,13 +1,28 @@
 package thedoctors05.tentfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button AddTent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AddTent = (Button) findViewById(R.id.bAddTent);
+        AddTent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),AddTent.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
