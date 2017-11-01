8fa25af4dfca614e6afb4581440ce74e7d73a582
Gruszecki <wojtek.gruszecki@gmail.com>
2017-11-01 20:40:13 +0100

    Backup

--- app/src/main/java/thedoctors05/tentfinder/Navigation.java
+++ app/src/main/java/thedoctors05/tentfinder/Navigation.java
@@ -1,6 +1,5 @@
 package thedoctors05.tentfinder;
 
-import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,28 +8,22 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Navigation extends AppCompatActivity implements SensorEventListener, LocationListener {
-    Double longitude = null;
-    Double latitude = null;
+    double longitude = 0.0;
+    double latitude = 0.0;
 
     ImageView arrow;
     TextView distance_text, coor, tentName;
 
-    Double currLon, currLat;
-    Double deltaX, deltaY, wbDeltaX, wbDeltaY, fi, azimuthTent, angle, distance;
+    double currLon, currLat;
+    double deltaX, deltaY, wbDeltaX, wbDeltaY, fi, azimuthTent, angle, distance;
     float azimuthPhone;
 
     LocationManager lm;
@@ -39,18 +32,12 @@ public class Navigation extends AppCompatActivity implements SensorEventListener
     Location loc;
     private SensorManager sm;
 
-    private int[] layouts;
-    private ViewPager viewPager;
-    private ViewPagerAdapter viewPagerAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
 
         arrow = (ImageView) findViewById(R.id.arrow);
-        tentName = (EditText) findViewById(R.id.nazwaNamiotu_et);
-        arrow.setVisibility(View.INVISIBLE);
         tentName = (TextView) findViewById(R.id.nazwaNamiotu_tv);
         distance_text = (TextView) findViewById(R.id.distance_text);
         coor = (TextView) findViewById(R.id.coor);
@@ -58,23 +45,12 @@ public class Navigation extends AppCompatActivity implements SensorEventListener
         if (savedInstanceState == null) {
             Bundle extras = getIntent().getExtras();
             String title = extras.getString("name");
+            tentName.setText(title);
             longitude = Double.parseDouble(extras.getString("long"));
             latitude = Double.parseDouble(extras.getString("lati"));
-            tentName.setText(title);
         }
 
-        arrow.setVisibility(View.INVISIBLE);
         navigationStart();
-
-        viewPager = (ViewPager) findViewById(R.id.view_pager);
-        viewPagerAdapter = new ViewPagerAdapter();
-        viewPager.setAdapter(viewPagerAdapter);
-        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
-
-        layouts = new int[] {
-                R.layout.activity_navigation,
-                R.layout.activity_test
-        };
     }
 
     public void navigationStart () {
@@ -94,7 +70,7 @@ public class Navigation extends AppCompatActivity implements SensorEventListener
         rotationCalculate();
         distanceCalculate();    //Order must be keeped
 
-        arrow.setRotation(angle.floatValue());
+        arrow.setRotation((float)angle);
 
         distance_text.setText(distance + "");
         coor.setText(azimuthPhone + "");
@@ -142,32 +118,6 @@ public class Navigation extends AppCompatActivity implements SensorEventListener
         loc = lm.getLastKnownLocation(bestProvider);
     }
 
-    private int getItem(int i) {
-        return viewPager.getCurrentItem() + i;
-    }
-
-    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
-
-        @Override
-        public void onPageSelected(int position) {
-
-            switch (position){
-                case 0:
-                    arrow.setVisibility(View.VISIBLE);
-                    break;
-                case 1:
-                    arrow.setVisibility(View.INVISIBLE);
-                    break;
-            }
-        }
-
-        @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {}
-
-        @Override
-        public void onPageScrollStateChanged(int arg0) {}
-    };
-
     public void onLocationChanged (Location location) {
         refreshLoc();
 
@@ -193,7 +143,7 @@ public class Navigation extends AppCompatActivity implements SensorEventListener
         azimuthPhone = event.values[0];
 
         rotationCalculate();
-        arrow.setRotation(angle.floatValue());
+        arrow.setRotation((float)angle);
         coor.setText("Phone az.: " + azimuthPhone + "\nTent az.: " + azimuthTent + "\nAngle: " + angle);
     }
 
@@ -205,41 +155,4 @@ public class Navigation extends AppCompatActivity implements SensorEventListener
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-    /**
-     * View pager adapter
-     */
-    public class ViewPagerAdapter extends PagerAdapter {
-        private LayoutInflater layoutInflater;
-
-        public ViewPagerAdapter() {
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-            View view = layoutInflater.inflate(layouts[position], container, false);
-            container.addView(view);
-
-            return view;
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object obj) {
-            return view == obj;
-        }
-
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            View view = (View) object;
-            container.removeView(view);
-        }
-    }
 }
